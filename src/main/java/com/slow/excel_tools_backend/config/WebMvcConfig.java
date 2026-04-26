eac3e37219ac6a4311ba40b0b8f8a1060236293d
liusenlong <Admin!@#123>
2026-04-26 11:26:53 +0800

    fix: remove interceptor for auth/login

--- src/main/java/com/slow/excel_tools_backend/config/WebMvcConfig.java
+++ src/main/java/com/slow/excel_tools_backend/config/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package com.slow.excel_tools_backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -9,18 +8,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
-    private final AuthInterceptor authInterceptor;
-
-    public WebMvcConfig(AuthInterceptor authInterceptor) {
-        this.authInterceptor = authInterceptor;
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        // 只拦截小程序的用户相关接口，不拦截管理后台接口
-        registry.addInterceptor(authInterceptor)
-                .addPathPatterns("/api/auth/login")
-                .excludePathPatterns();
-    }
 }
